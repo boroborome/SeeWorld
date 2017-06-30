28f0aec9094d30b27bd3223a94ceee494478a6ef
Yushan Gao <boroborome@sina.com>
2017-06-30 15:21:10 +0800

    remove auto config of database

--- src/main/java/com/happy3w/seeworld/SeeworldApplication.java
+++ src/main/java/com/happy3w/seeworld/SeeworldApplication.java
@@ -4,7 +4,11 @@ import com.happy3w.seeworld.job.AnalyzeJob;
 import com.happy3w.seeworld.job.DownloadJob;
 import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -20,6 +24,10 @@ import java.util.Map;
 
 @SpringBootApplication
 @EnableElasticsearchRepositories(basePackages = "org/springframework/data/elasticsearch/repositories")
+@EnableAutoConfiguration(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class})
 public class SeeWorldApplication {
 
 	@Bean
