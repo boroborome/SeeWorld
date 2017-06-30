162221bba9ccf9f9814bd5e30f5d7d6add6300a6
Yushan Gao <boroborome@sina.com>
2017-06-30 15:20:43 +0800

    refresh download test

--- src/test/java/com/happy3w/seeworld/task/DownloadTaskTest.java
+++ src/test/java/com/happy3w/seeworld/task/DownloadTaskTest.java
@@ -1,18 +1,56 @@
 package com.happy3w.seeworld.task;
 
-import com.happy3w.seeworld.entity.SystemConfig;
+import com.happy3w.seeworld.SeeWorldApplication;
+import com.happy3w.seeworld.job.AnalyzeJob;
+import com.happy3w.seeworld.job.DownloadJob;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
 
 /**
  * Created by ysgao on 28/06/2017.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SeeWorldApplication.class)
+//@TestConfiguration
+//@MockBean(RabbitTemplate.class)
 public class DownloadTaskTest {
+
+    @Autowired
+    private DownloadJob downloadJob;
+
+//    @Autowired
+//    @SpyBean
+    @MockBean
+    private RabbitTemplate rabbitTemplate;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
     public void should_download_success() {
-        DownloadTask task = new DownloadTask();
-        String content = task.download("http://www.sina.com.cn");
-        Assert.assertNotNull(content);
-        Assert.assertTrue(content.length() > 0);
+        downloadJob.receiveMessage("http://www.sina.com.cn");
+
+        ArgumentCaptor<String> captorQueue = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> captorContent = ArgumentCaptor.forClass(String.class);
+        Mockito.verify(rabbitTemplate)
+                .convertAndSend(captorQueue.capture(), captorContent.capture());
+
+        Assert.assertEquals(AnalyzeJob.Queue, captorQueue.getValue());
+        Assert.assertNotNull(captorContent.getValue());
+        Assert.assertTrue(captorContent.getValue().length() > 0);
     }
 }
