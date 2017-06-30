1daed3edbb551c00368c8d77219fa87c29648a20
Yushan Gao <boroborome@sina.com>
2017-06-30 15:21:29 +0800

    refact

--- src/main/java/com/happy3w/seeworld/job/AnalyzeJob.java
+++ src/main/java/com/happy3w/seeworld/job/AnalyzeJob.java
@@ -6,13 +6,6 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-
 /**
  * Created by ysgao on 28/06/2017.
  */
